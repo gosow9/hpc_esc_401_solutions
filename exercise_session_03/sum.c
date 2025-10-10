9a0244cf94ed8ee8984f88a5a30307a9bfcd65f3
Cedric Renda <crenda@eiger-ln002.cscs.ch>
2025-10-10 10:56:59 +0200

    add paralesiation

--- exercise_session_03/sum.c
+++ exercise_session_03/sum.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
+#include <omp.h>
+#include "gettime.h"
 
 typedef struct {
     float x;
@@ -14,12 +16,14 @@ int main(int argc, char *argv[]) {
     const int N = 1024*1024*256;
     coord *data;
     int i;
-    double sum;
+    double sum,t1,t2;
+    t1 = getTime();
     
     data = malloc(N * sizeof(coord));
     assert(data);
     
     sum = 0.0;
+    #pragma omp parallel for reduction(+ : sum)
     for(i=0; i<N; ++i) {
         data[i].x = i & 31;
         data[i].y = i & 63;
@@ -28,5 +32,7 @@ int main(int argc, char *argv[]) {
         sum += sqrt(data[i].r2);
     }
     printf("sum=%f\n", sum);
+    t2 = getTime()-t1;
+    printf("time = %.6f \n",t2);
     return 0;
 }
